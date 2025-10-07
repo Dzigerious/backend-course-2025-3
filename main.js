cafa0ac79b8469f7861d561907b136d74adceb87
Струмінський Захарій bc2025-3 <zakhar.struminskiy.prog@gmail.com>
2025-10-07 09:28:22 +0300

    Added updated main.js

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 program
   .requiredOption('-i, --input <path>', 'Input JSON file')
   .option('-o, --output <path>', 'Output file')
-  .option('-d, --display', 'Display result in console');
+  .option('-d, --display', 'Display result in console')
+  .option('-f, --furnished', 'Show only furnished houses')
+  .option('-p, --price <number>', 'Show houses with price less than given', parseInt)
+  .option('-b, --bedrooms <number>', 'Show houses with exact number of bedrooms', parseInt);
 
 program.parse(process.argv);
-
 const options = program.opts();
 
 if (!options.input) {
@@ -20,11 +22,28 @@ if (!fs.existsSync(options.input)) {
   process.exit(1);
 }
 
-const data = JSON.parse(fs.readFileSync(options.input, 'utf-8'));
-let result = data;
+let data = JSON.parse(fs.readFileSync(options.input, 'utf-8'));
+
+// --- фільтри ---
+if (options.furnished) {
+  data = data.filter(item => item.furnishingstatus === "furnished");
+}
+
+if (options.price) {
+  data = data.filter(item => parseInt(item.price) < options.price);
+}
+
+if (options.bedrooms) {
+  data = data.filter(item => parseInt(item.bedrooms) === options.bedrooms);
+}
+
+// --- результат: ціна | площа | кімнати | меблі ---
+const result = data.map(item => {
+  return `${item.price} | ${item.area} sqft | ${item.bedrooms} rooms | ${item.furnishingstatus}`;
+}).join('\n');
 
 if (options.output) {
-  fs.writeFileSync(options.output, JSON.stringify(result, null, 2));
+  fs.writeFileSync(options.output, result);
 }
 
 if (options.display) {
